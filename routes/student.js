bce22d3dd41239f4e45f7042774bd22f6dc5c7e9
강현승 <khskeb0513@gmail.com>
2020-08-30 16:03:30 +0900

    app.js - 쿼리 처리 및 json문법 변경

--- routes/student.js
+++ routes/student.js
@@ -3,16 +3,19 @@ const router = express.Router();
 const {student} = require('../models')
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
+router.get('/student', function (req, res, next) {
     const query = {
-        name: req.query['name']
+        name: req.query['name'] || null
     }
     student.findAll({
         where: {
             name: query.name
         }
     }).then(r => {
-        res.json(r)
+        res.json({
+            success: true,
+            payload: r
+        })
     }, e => {
         console.error(e)
         res.json({
